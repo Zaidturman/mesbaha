9c243751bfc7002aa25d38977b62c0279d0ad75b
zaidturman <zaidturman17@gmail.com>
2024-11-10 22:19:41 +0200

    structerd done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
-  sidebar.style.display = "flex";
+  sidebar.style.display = "block";
 }
 
 function hideSidebar() {
@@ -8,29 +8,4 @@ function hideSidebar() {
   sidebar.style.display = "none";
 }
 
-let count = 0;
-const maxCount = 33;
-const words = [
-  "سبحان الله",
-  "الحمد لله",
-  "لا إله إلا الله",
-  "الله أكبر",
-  "استغفر الله",
-];
-let currentWordIndex = 0;
 
-const counterElement = document.getElementById("counter");
-const wordElement = document.getElementById("word");
-const incrementButton = document.getElementById("increment-btn");
-
-incrementButton.addEventListener("click", () => {
-  count++;
-
-  if (count >= maxCount) {
-    count = 0; // إعادة تصفير العداد
-    currentWordIndex = (currentWordIndex + 1) % words.length; // تغيير الكلمة
-    wordElement.textContent = words[currentWordIndex];
-  }
-
-  counterElement.textContent = count;
-});
